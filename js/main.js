10371d4223e55828a7d09ffad9ecc828cdedcd86
Denis Nesterov <den_nesterov_1992@mail.ru>
2022-09-22 18:29:31 +0500

    Complite all tasks HW1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, imgUrl: 'https://img.mvideo.ru/Big/30062055bb.jpg'},
+    {id: 2, title: 'Mouse', price: 20, imgUrl: 'https://img.mvideo.ru/Big/50133149bb.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, imgUrl: 'https://img.mvideo.ru/Big/50168992bb.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, /* url: 'https://img.mvideo.ru/Big/40078743bb.jpg' */},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+
+const imgPlaceholder = 'https://www.rassvet-td.ru/files/c7fdd594-44d9-11e1-b24c-001e90f915e2_cb22a654-172e-11eb-87cb-d050999b4264.jpeg'
+const renderProduct = (title, price, imgUrl = imgPlaceholder) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
+                <img height='240px' src='${imgUrl}'>
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = list
+        .map(item => renderProduct(item.title, item.price, item.imgUrl))
+        .join(''); // Массив выводился на страницу по умолчанию через запятые. Исправил это методом join.
 };
 
 renderPage(products);
